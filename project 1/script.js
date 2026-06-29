b2c5ee7e715fb8fd0f63b669f39c3adef09d97f1
Vishnu <vishnunandhu199@gmail.com>
2026-06-30 00:23:04 +0530

    Add files via upload
    1

---
+++ project 1/script.js
@@ -0,0 +1,7 @@
+const orderBtn = document.getElementById('orderBtn');
+
+if (orderBtn) {
+  orderBtn.addEventListener('click', () => {
+    alert('Your South Tiffin order has been placed!');
+  });
+}
